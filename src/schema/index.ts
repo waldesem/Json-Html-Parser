9f8cc0eac0bf5e86f32a2748aaa23beb48fc52b3
Vladimir Semenenko <wsemenenko@gmail.com>
2026-07-08 15:10:34 +0300

    Create repo

---
+++ src/schema/index.ts
@@ -0,0 +1,80 @@
+import { localStr } from '@/utils'
+import { Schemas } from '@/types'
+
+export const schema: Schemas[] = [
+  { keys: ['positionName', 'department'], tag: 'caption' },
+  { keys: ['lastName', 'firstName', 'midName'], tag: 'thead' },
+  {
+    key: 'birthday',
+    label: 'Дата рождения',
+    foo: (row: string) => localStr(row),
+  },
+  { key: 'birthplace', label: 'Место рождения' },
+  { key: 'citizen', label: 'Гражданство' },
+  { key: 'additionalCitizenship', label: 'Двойное гражданство' },
+  { key: 'snils', label: 'СНИЛС' },
+  { key: 'inn', label: 'ИНН' },
+  { key: 'maritalStatus', label: 'Семейное положение' },
+  { label: 'Адрес' },
+  { key: 'regAddress', label: 'Адрес регистрации' },
+  { key: 'validAddress', label: 'Адрес проживания' },
+  { label: 'Контакты' },
+  { key: 'contactPhone', label: 'Телефон' },
+  { key: 'email', label: 'Email' },
+  { label: 'Документы' },
+  { key: 'passportSerial', label: 'Серия документа' },
+  { key: 'passportNumber', label: 'Номер документа' },
+  { key: 'passportIssuedBy', label: 'Кем выдан' },
+  {
+    key: 'passportIssueDate',
+    label: 'Дата',
+    foo: (row: string) => localStr(row),
+  },
+  {
+    key: 'education',
+    label: 'Образование',
+    items: [
+      { key: 'educationType', label: 'Уровень', attr: "class='fw-semibold'" },
+      { key: 'institutionName', label: 'Учебное заведение' },
+      { key: 'endYear', label: 'Год окончания' },
+      { key: 'specialty', label: 'Специальность' },
+    ],
+  },
+  {
+    key: 'experience',
+    label: 'Работа',
+    items: [
+      { key: 'name', label: 'Место работы', attr: "class='fw-semibold'" },
+      { key: 'beginDate', label: 'Приём', foo: (row: string) => localStr(row) },
+      {
+        key: 'endDate',
+        label: 'Увольнение',
+        foo: (row: string) => localStr(row),
+      },
+      { key: 'currentJob', label: 'Текущая работа' },
+      { key: 'position', label: 'Должность' },
+      { key: 'address', label: 'Адрес' },
+      { key: 'fireReason', label: 'Причина увольнения' },
+    ],
+  },
+  {
+    key: 'nameWasChanged',
+    label: 'Перемена имени',
+    items: [
+      { key: 'lastNameBeforeChange', label: 'Фамилия' },
+      { key: 'firstNameBeforeChange', label: 'Имя' },
+      { key: 'midNameBeforeChange', label: 'Отчество' },
+      { key: 'changed', label: 'Год изменения' },
+      { key: 'reason', label: 'Причина' },
+    ],
+  },
+  {
+    key: 'organizations',
+    label: 'Участие в коммерческой деятельности',
+    items: [
+      { key: 'name', label: 'Название', attr: "class='fw-medium'" },
+      { key: 'activity', label: 'Вид деятельности' },
+      { key: 'inn', label: 'ИНН' },
+    ],
+  },
+]
---
+++ src/types/index.ts
@@ -0,0 +1,77 @@
+export interface Education {
+  educationType: string
+  institutionName?: string
+  endYear?: string
+  specialty?: string
+}
+
+export interface Expirience {
+  name: string
+  beginDate: string
+  endDate?: string
+  currentJob?: string
+  position: string
+  address?: string
+  fireReason?: string
+}
+
+export interface Names {
+  lastNameBeforeChange?: string
+  firstNameBeforeChange?: string
+  midNameBeforeChange?: string
+  changed?: string
+  reason?: string
+}
+
+export interface Organizations {
+  name: string
+  activity: string
+  inn?: string
+}
+
+export interface Conditions {
+  education: Education[]
+  experience: Expirience[]
+  nameWasChanged: Names[]
+  organizations: Organizations[]
+}
+
+export interface Data {
+  positionName: string
+  department?: string
+  lastName: string
+  firstName: string
+  midName?: string
+  birthday: string
+  birthplace?: string
+  citizen: string
+  additionalCitizenship?: string
+  snils?: string
+  inn?: string
+  maritalStatus?: string
+  regAddress?: string
+  validAddress?: string
+  contactPhone?: string
+  email?: string
+  passportSerial?: string
+  passportNumber?: string
+  passportIssuedBy?: string
+  passportIssueDate?: string
+}
+
+export interface Items {
+  key: string
+  label: string
+  foo?: Function
+  attr?: string
+}
+
+export interface Schemas {
+  key?: keyof Data | keyof Conditions
+  keys?: (keyof Data)[]
+  label?: string
+  tag?: 'caption' | 'thead'
+  foo?: Function
+  attr?: string
+  items?: Items[]
+}
